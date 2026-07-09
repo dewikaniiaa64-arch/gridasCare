80e5247389af1c71a41fffff5d69bc3ac3fd9ad3
dewikaniiaa64-arch <dewikaniiaa64@gmail.com>
2026-07-09 11:55:52 +0700

    Project awal Next.js

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex flex-col justify-center items-center bg-sky-100">
+      <h1 className="text-5xl font-bold text-blue-700">
+        Website gridasCare
+      </h1>
+
+      <p className="mt-4 text-xl text-gray-700">
+        Selamat Datang di Website UKS SMKN 2 Sumedang
+      </p>
+
+      <button className="mt-8 bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700">
+        Masuk
+      </button>
+    </main>
+  );
+}
